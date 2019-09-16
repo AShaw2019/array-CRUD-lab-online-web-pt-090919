4c48eff5b0dd60b58c2e04f2ef26e9773d93a13e
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 01:57:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   my_array = ["wow", "wedding", "planning", "is", "stressful!"]
-  
+  my_array.first
 end
 
 def retrieve_last_element_from_array(array)
