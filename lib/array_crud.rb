c87509317b9fe13022c9433c1f1553b1eac36908
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 01:55:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
    my_array = ["wedding", "planning", "is", "stressful!"]
-   my_array
+   my_array[0]
 end
 
 def retrieve_first_element_from_array(array)
