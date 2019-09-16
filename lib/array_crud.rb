0998bc11c5dc987ec5eba1c767d58a983b88948e
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 01:55:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-   my_array = ["wedding", "planning", "is", "stressful!"]
+   my_array = ["wow", "wedding", "planning", "is", "stressful!"]
    wow = my_array.shift
 end
 
