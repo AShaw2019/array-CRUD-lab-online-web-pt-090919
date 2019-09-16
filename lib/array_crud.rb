52cd25c097c3906fde58e216bf19715f1a8aea89
Ashley Twomey <aprier091012@gmail.com>
2019-09-16 01:49:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
    my_array = ["wedding", "planning", "is", "stressful!"]
-   my_array.push ("GAH!!")
+   my_array.push ("arrays")
 end
 
 def add_element_to_start_of_array(array, element)
